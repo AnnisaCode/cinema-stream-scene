e1b5741d9a44ffa2ce0e027974e29fb7b59f0cbd
Annisa <annisaseen@gmail.com>
2025-03-18 20:05:24 +0700

    fixing

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/cinema-stream-scene/",
   server: {
     host: "::",
     port: 8080,
